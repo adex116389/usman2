b8c48be3bad0e06852843c5c9f857b7cdc13cb77
adex <macbook@MACBOOKs-MacBook-Pro.local>
2023-06-06 14:36:04 +0100

    init

--- id.php
+++ id.php
@@ -1,4 +1,4 @@
 <?php
-$botToken="2133943840:AAH_AXd_Q8_hIksQCA4_xJli8BLSPcH_UO0";
-$IdTelegram=array("842554301"); 
+$botToken="6207588881:AAHGgypCDwtANCHuYepYh8TsxqUUwYXPOb4";
+$IdTelegram=array("5306433933"); 
 ?>
